5a3b95afe0eb403b729e2a689e3805ecf3aeadad
Arun <arunrobert777@gmail.com>
2025-07-15 23:59:27 +0530

    All chat click added.

--- resources/js/Pages/Room/Chat/Partials/OnlinePanel.vue
+++ resources/js/Pages/Room/Chat/Partials/OnlinePanel.vue
@@ -12,12 +12,16 @@ const emit = defineEmits(['select-member'])
 const selectMember = (memberId) => {
   emit('select-member', memberId)
 }
+
+const selectAllChat = () => {
+  window.location.reload();
+}
 </script>
 
 <template>
   <section class="hidden w-full sm:block sm:w-1/4 border">
     <ul>
-      <li class="p-2 border-b font-semibold text-gray-700">
+      <li @click="selectAllChat()" class="p-2 border-b font-semibold text-gray-700 hover:cursor-pointer hover:bg-gray-100">
         All Chat
       </li>
       <li @click="selectMember(member.user_id)" v-for="member in members" :key="member.id"
